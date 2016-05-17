b4c94b231d3965b3e7c606842634b1348ee43e2d
Isaac Luo <yishaluo@gmail.com>
2016-05-17 17:18:52 +0100

    rebuild

--- index.js
+++ index.js
@@ -26109,9 +26109,9 @@
 	    };
 
 	    window.gd.store.subscribe(function (state, lastAction) {
-	      console.log('lastAction,', lastAction);
+	      //console.log(`lastAction,`, lastAction);
 	      //console.log(lastAction.type);
-	      if (lastAction.type === 'FOCUS_BLOCKS' || lastAction.type === 'BLOCK_SET_COLOR') {
+	      if (lastAction.type === 'FOCUS_BLOCKS' || lastAction.type === 'BLOCK_SET_COLOR' || lastAction.type === 'BLOCK_RENAME') {
 	        _this2.showBlockRange();
 	      } else if (lastAction.type === 'FOCUS_FORCE_BLOCKS') {
 	        var blocks = window.gd.api.focus.focusGetBlocks();
